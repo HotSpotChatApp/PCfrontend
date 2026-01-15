d79d30528cac6a311434292691cbd56565c3c8aa
HotSpotChatApp <hotspotchatapp@gmail.com>
2026-01-15 15:47:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: [
+      'pcfrontend.onrender.com'
+    ]
   },
   build: {
     outDir: 'dist',
